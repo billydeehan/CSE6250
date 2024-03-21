fd7c807ecb001b2eb4ac98150108d84e12ebae2b
billydeehan <61507349+billydeehan@users.noreply.github.com>
2024-03-21 12:52:11 -0500

    bdd updates

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -1,8 +1,20 @@
+import pandas as pd
+import os
+
 def load_data():  
-    pass
+    print('test')
+    file_path = 'data/covid-liver.csv'
+    df = pd.read_csv(file_path, sep='\t')
+    print(df.head())
+    # return df
+    return 'test'
 def clean_data(): 
     pass 
 def impute_missing_values():
     pass
 def merge_data_sources():
     pass
+
+df = load_data()
+
+print(os.getcwd())
